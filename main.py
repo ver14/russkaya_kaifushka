ed647dd08f3d170dd11c2494649d5b507b39663f
ver14 <af1566620@gmail.com>
2025-10-03 00:31:30 +0300

    add Bazizjone class

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+class Bazizjone:
+    bazizjone = True
+
+    def is_bazizjone(self, status: bool) -> Optional[str]:
+        if status:
+            return 'РУССКАЯ КАЙФУШКА'
+        else:
+            return None
